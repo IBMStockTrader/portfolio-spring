7385eeda32fa43d700d91042bf092b25efe9f7b0
Ozzy Osborne <ozzy@ca.ibm.com>
2018-12-05 13:44:15 -0500

    Add extra mapping for frontend usage

--- src/main/java/com/ibm/hybrid/cloud/sample/portfolio/controllers/RestEndpoints.java
+++ src/main/java/com/ibm/hybrid/cloud/sample/portfolio/controllers/RestEndpoints.java
@@ -28,19 +28,18 @@ import org.modelmapper.TypeToken;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/portfolio")
 public class RestEndpoints {
 
     @Autowired
@@ -50,7 +49,7 @@ public class RestEndpoints {
 
     ModelMapper mapper = new ModelMapper();
 
-    @GetMapping("/")
+    @GetMapping({"","/"}) //"" workaround for ui requesting /portfolio instead of /portfolio/
     @Secured({"ROLE_STOCKVIEWER","ROLE_STOCKTRADER"})
     public List<Portfolio> getAllPortfolios() { 
         return mapper.map(portfolioService.getAllPortfolios(), new TypeToken<List<Portfolio>>() {}.getType());
