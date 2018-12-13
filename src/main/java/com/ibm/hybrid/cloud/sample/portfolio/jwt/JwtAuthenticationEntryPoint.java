a625d77b8733d6b3692ca56132ea5429ec522ac9
Ozzy Osborne <ozzy@ca.ibm.com>
2018-12-13 15:10:20 -0500

    Example error handling

--- src/main/java/com/ibm/hybrid/cloud/sample/portfolio/jwt/JwtAuthenticationEntryPoint.java
+++ src/main/java/com/ibm/hybrid/cloud/sample/portfolio/jwt/JwtAuthenticationEntryPoint.java
@@ -18,8 +18,13 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -28,23 +33,64 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @Component
 @ControllerAdvice
 public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+  private final static Logger logger = LoggerFactory.getLogger(JwtAuthenticationEntryPoint.class);
+
   @Override
   public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException ) throws IOException, ServletException { 
-    handleDeniedException(response,authException);
+    handleDeniedException(request,response,authException);
   }
 
   @ExceptionHandler(value = { AccessDeniedException.class })
   public void commence(HttpServletRequest request, HttpServletResponse response, AccessDeniedException ex ) throws IOException {
-    handleDeniedException(response,ex);
+    handleDeniedException(request, response,ex);
   }
 
-  private void handleDeniedException(HttpServletResponse response, Exception ex) throws IOException{
-    
-    //driven with InsufficientAuthenticationException the JWT is absent, badly signed, etc
-    //driven with AccessDeniedException if the method was @Secured (or equiv) with a role the JWT did not grant.
+  private String sanitise(String msg, String jwt) {
+    if (jwt !=null && msg.contains(jwt) ){
+      msg = msg.replace(jwt, "");
+    }
+    return msg;
+  }
 
+  /**
+    * driven with InsufficientAuthenticationException the JWT is absent, badly signed, etc
+    * driven with AccessDeniedException if the method was @Secured (or equiv) with a role the JWT did not grant.
+    */
+  private void handleDeniedException(HttpServletRequest request, HttpServletResponse response, Exception ex) throws IOException{
+
+
+    String authHeader = request.getHeader("Authorization");
+    String jwt = null;
+    if(authHeader!=null){
+      String parts[] = authHeader.split(" ");
+      if(parts.length >1){
+        jwt = parts[1];
+      }
+    }
+
+    //log upstream reason for failure.
+    String exMsg = ex.getClass().getSimpleName()+" : "+sanitise(ex.getMessage(),jwt);
+    Throwable t = ex;
+    while(t.getCause()!=null){
+      t = t.getCause();
+      exMsg += "("+t.getClass().getSimpleName()+" : "+sanitise(t.getMessage(),jwt)+")";
+    }
+
+    //AccessDenied means user auth was accepted, but permissions check failed.
+    //log user & perm.
+    if(ex instanceof AccessDeniedException){
+      SecurityContext sc = SecurityContextHolder.getContext();
+      Authentication a = sc.getAuthentication();
+      exMsg += "(User:"+a.getName()+" Auth:"+a.getAuthorities()+")";
+    }
+
+    //Record failed attempt
+    logger.warn("Failed authentication attempt for resource "+request.getRequestURL()+" "+exMsg);
+
+    //Response for user. Adjust detail as required.
     response.setContentType("application/json");
     response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-    response.getOutputStream().println("{ \"error\": \"" + ex.getMessage() + "\" }");
+    response.getOutputStream().println("{ \"error\": \"" + sanitise(ex.getMessage(),jwt) + "\" }");
   }
 }
